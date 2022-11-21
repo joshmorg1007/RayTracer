8e5be4888f01cf988d682c5fa84216e5c4cfc56e
Josh Morgan <eston1007@gmail.com>
2022-11-21 14:00:58 -0800

    added basic outline for Sphere class

---
+++ lib/csc305/Sphere.cpp
@@ -0,0 +1,47 @@
+#include "Sphere.hpp"
+
+namespace csc305{
+  //Public
+
+  // Default Constructor
+  Sphere::Sphere(){
+    pos_ = new glm::vec3(0,0,0);
+    scale_ = new glm::vec3(0,0,0);
+    color_ = new glm::vec3(0,0,0);
+    Ka_ = 0.0;
+    Kd_ = 0.0;
+    Ks_ = 0.0;
+    Kr_ = 0.0;
+    specExp_ = 1;
+  }
+
+  //main Constructor
+  Sphere::Sphere(glm::vec3 pos, glm::vec3 scale, glm::vec3 color, float Ka_, float Kd_, float Ks_, float Kr_, int specExp_){
+    pos_ = pos;
+    scale_ = scale;
+    color_ = color;
+    Ka_ = Ka;
+    Kd_ = Kd;
+    Ks_ = Ks;
+    Kr_ = Kr;
+    specExp_ = specExp;
+  }
+
+  //Move Constructor
+  Sphere::Sphere(Sphere && other) = default;
+
+  //Move Operator
+  Sphere::Sphere & operator=(Sphere && other) = default;
+
+  //Copy Constructor
+  Sphere::Sphere(const Sphere &) = default;
+
+  //Copy Operator
+  Sphere::Sphere & operator=(const Sphere &) = default;
+
+  //Deconstructor
+  Sphere::~Sphere() = default;
+
+
+  //Private
+}
