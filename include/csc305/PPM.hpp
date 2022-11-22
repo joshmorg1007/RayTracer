420e613597f7164669eae31c5d33e2804d5a0c10
Josh Morgan <eston1007@gmail.com>
2022-11-22 13:31:59 -0800

    implemented PPM Class

---
+++ include/csc305/PPM.hpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#ifndef ppm_h
+#define ppm_h
+namespace csc305{
+    class PPM{
+    public:
+
+      //Default Constructor
+      PPM();
+
+      //Main Constructor
+      PPM(int width, int height, unsigned char* pixels);
+
+      //Move Operator
+      PPM & operator=(PPM && other);
+
+      //Copy Constructor
+      PPM(const PPM & other);
+
+      //Copy Operator
+      PPM & operator=(const PPM & other);
+
+      //Deconstructor
+      ~PPM();
+
+      //Save Image
+      // Modified From Given Code
+      void save_imageP3(char* fname);
+
+    private:
+      //Attributes
+      int height_;
+      int width_;
+      unsigned char* pixels_;
+    }
+}
+#endif
---
+++ lib/csc305/PPM.cpp
@@ -0,0 +1,92 @@
+#include "PPM.hpp"
+namespace csc305{
+  //Public
+
+  //Default Constructor
+  PPM::PPM(){
+    pixels_ = nullptr;
+    height_ = 0;
+    width_ = 0;
+  }
+
+  // Main Constructor
+  PPM::PPM(int width, int height, unsigned char* pixels){
+    width_ = width;
+    height_ = height_;
+    pixels_ = pixels;
+  }
+
+  //Move Constructor
+  PPM::PPM(PPM && other){
+    delete [] pixels_;
+    pixels_ = others.pixels_;
+    other.pixels_ = nullptr;
+    height_ = other.height_;
+    width_ = other.width_;
+  }
+
+  //Move Operator
+  PPM & PPM::operator=(PPM && other){
+    delete [] pixels_;
+    pixels_ = others.pixels_;
+    other.pixels_ = nullptr;
+    height_ = other.height_;
+    width_ = other.width_;
+    return *this;
+  }
+
+  //Copy Constructor
+  PPM::PPM(const PPM & other){
+    delete [] pixels_;
+    height_ = other.height_;
+    width_ = other.height_;
+    pixels_ = new unsigned char[height_ * width_ * 3];
+    for(i = 0; i < height_*width_*3; i++){
+      pixels_[i] = other.pixels_[i];
+    }
+  }
+
+  //Copy Operator
+  PPM & PPM::operator=(const PPM & other){
+    delete [] pixels_;
+    height_ = other.height_;
+    width_ = other.height_;
+    pixels_ = new unsigned char[height_ * width_ * 3];
+    for(i = 0; i < height_*width_*3; i++){
+      pixels_[i] = other.pixels_[i];
+    }
+    return *this;
+  }
+
+  void PPM::save_imageP3(char* fname) {
+    FILE *fp;
+    const int maxVal=255;
+
+    printf("Saving image %s: %d x %d\n", fname,width_,height_);
+    fp = fopen(fname,"w");
+    if (!fp) {
+      printf("Unable to open file '%s'\n",fname);
+      return;
+    }
+    fprintf(fp, "P3\n");
+    fprintf(fp, "%d %d\n", width_, height_);
+    fprintf(fp, "%d\n", maxVal);
+
+    int k = 0 ;
+    for(int j = 0; j < height_; j++) {
+
+      for( int i = 0 ; i < width_; i++)
+      {
+        fprintf(fp," %d %d %d", pixels_[k],pixels_[k+1],pixels_[k+2]) ;
+        k = k + 3 ;
+      }
+      fprintf(fp,"\n") ;
+    }
+    fclose(fp);
+  }
+
+  //Deconstructor
+  PPM:~PPM(){
+    delete [] pixels;
+  }
+}
